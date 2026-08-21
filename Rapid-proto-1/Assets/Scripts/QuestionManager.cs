52a1762aa8fa2fde9b7da50c48c53971e3d6134d
AfeefAhmer <s2401102@edu.bc.fi>
2026-08-21 09:53:09 +0300

    UI napit valmis versio

--- Rapid-proto-1/Assets/Scripts/QuestionManager.cs
+++ Rapid-proto-1/Assets/Scripts/QuestionManager.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class QuestionManager : MonoBehaviour
@@ -16,93 +15,133 @@ public class QuestionManager : MonoBehaviour
     public GameObject passButton;
     public GameObject rejectButton;
 
+    private NPCQuestion currentNPC;
     private int selectedAnswer = 0;
-
-    // Oikea kysymys
-    private int correctAnswer = 1;
 
     private void Start()
     {
-        // Aseta kysymykset suoraan nappeihin
-        SetButtonText(answerButton1, "Kuka olet?");
-        SetButtonText(answerButton2, "Mitä täällä tapahtuu?");
-        SetButtonText(answerButton3, "Voitko auttaa minua?");
-
-        // Piilota Pass ja Reject alussa
         passButton.SetActive(false);
         rejectButton.SetActive(false);
     }
 
-    // Laittaa tekstin Buttonin TMP-tekstiin
+    public void SetNPC(GameObject npc)
+    {
+        currentNPC = npc.GetComponent<NPCQuestion>();
+
+        if (currentNPC == null)
+        {
+            Debug.LogError(
+                "NPC:ltä " + npc.name +
+                " puuttuu NPCQuestion!"
+            );
+
+            return;
+        }
+
+        selectedAnswer = 0;
+
+        Debug.Log("Uusi NPC: " + npc.name);
+        Debug.Log("Kysymys 1: " + currentNPC.question1);
+        Debug.Log("Kysymys 2: " + currentNPC.question2);
+        Debug.Log("Kysymys 3: " + currentNPC.question3);
+
+        // Asetetaan tekstit nappeihin
+        SetButtonText(answerButton1, currentNPC.question1);
+        SetButtonText(answerButton2, currentNPC.question2);
+        SetButtonText(answerButton3, currentNPC.question3);
+
+        // Näytä kysymysnapit
+        answerButton1.SetActive(true);
+        answerButton2.SetActive(true);
+        answerButton3.SetActive(true);
+
+        guessButton.SetActive(true);
+
+        passButton.SetActive(false);
+        rejectButton.SetActive(false);
+    }
+
     private void SetButtonText(GameObject buttonObject, string text)
     {
-        TMP_Text buttonText = buttonObject.GetComponentInChildren<TMP_Text>();
+        if (buttonObject == null)
+        {
+            Debug.LogError("Question Button puuttuu!");
+            return;
+        }
 
-        if (buttonText != null)
+        TMP_Text textComponent =
+            buttonObject.GetComponentInChildren<TMP_Text>(true);
+
+        if (textComponent == null)
         {
-            buttonText.text = text;
+            Debug.LogError(
+                "Buttonista " + buttonObject.name +
+                " ei löytynyt TMP_Text-komponenttia!"
+            );
+
+            return;
         }
-        else
-        {
-            Debug.LogError("Buttonista ei löytynyt TMP_Text-komponenttia!");
-        }
+
+        textComponent.text = text;
     }
 
-    // Kysymysnapit kutsuvat tätä
-    public void Answer(int question)
+    public void Answer1()
     {
-        selectedAnswer = question;
-
-        Debug.Log("Valittu kysymys: " + selectedAnswer);
+        selectedAnswer = 1;
     }
 
-    // ARVAA-nappi
+    public void Answer2()
+    {
+        selectedAnswer = 2;
+    }
+
+    public void Answer3()
+    {
+        selectedAnswer = 3;
+    }
+
     public void Guess()
     {
-        // Piilota kysymysnapit
+        // Kysymysnapit pois
         answerButton1.SetActive(false);
         answerButton2.SetActive(false);
         answerButton3.SetActive(false);
 
-        // Piilota ARVAA-nappi
+        // ARVAA pois
         guessButton.SetActive(false);
 
-        // Näytä PASS ja REJECT
+        // PASS ja REJECT näkyviin
         passButton.SetActive(true);
         rejectButton.SetActive(true);
 
-        Debug.Log("Valittu kysymys: " + selectedAnswer);
+        Debug.Log("ARVAA painettu. Valittu kysymys: " + selectedAnswer);
     }
 
-    // PASS
     public void Pass()
     {
-        if (selectedAnswer == correctAnswer)
-        {
-            Debug.Log("OIKEIN! PASS");
-        }
-        else
-        {
-            Debug.Log("VÄÄRIN!");
-        }
+        if (currentNPC == null)
+            return;
 
-        passButton.SetActive(false);
-        rejectButton.SetActive(false);
+        FinishNPC();
     }
 
-    // REJECT
     public void Reject()
     {
-        if (selectedAnswer != correctAnswer)
-        {
-            Debug.Log("OIKEIN! REJECT");
-        }
-        else
-        {
-            Debug.Log("VÄÄRIN!");
-        }
+        if (currentNPC == null)
+            return;
 
+        FinishNPC();
+    }
+
+    private void FinishNPC()
+    {
         passButton.SetActive(false);
         rejectButton.SetActive(false);
+
+        if (currentNPC != null)
+        {
+            Destroy(currentNPC.gameObject);
+            currentNPC = null;
+        }
     }
 }
