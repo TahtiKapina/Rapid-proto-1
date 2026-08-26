8aca4f143313439b973966e667da141da2a8723f
Daniel <s2401272@edu.bc.fi>
2026-08-26 10:24:36 +0300

    GhostData scripti aloitettu
    Aloitin Scriptin joka tarkistaa onko haamu Yokai

---
+++ Assets/Scripts/GhostData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GhostData : MonoBehaviour
+{
+    // Haamujen ominaisuudet
+    public bool isRobot;
+    public bool isHumanoid;
+    public bool hasRedEyes;
+    public bool isFloating;
+    public bool speaksOnlyWhenNeeded;
+
+
+    // Tarkistaa onko yokai
+    public bool IsYokai()
+    {
+        if (isRobot) return true;
+
+        if (hasRedEyes) return true;
+
+        return false;
+    }
+}
--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class Interact : MonoBehaviour
 {
     public Spawner spawner;
+    public AudioClip soundEffect;
 
     void Start()
     {
     }
     private void OnMouseDown()
     {
+        if (soundEffect != null)
+        {
+            AudioSource.PlayClipAtPoint(soundEffect, transform.position);
+        }
+
         Destroy(GameObject.FindWithTag("Character"));
         spawner.SpawnRandom();
     }
